638f192fba3651876a019edb9d16bf1e11ad906f
Đặng Minh Hoàng <Hoangsusp@gmail.com>
2025-07-25 16:45:38 +0700

    Added layout for each window

--- DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620871b3ade72395b403e54b3b776631e64e0dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
